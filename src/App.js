eba50a152ac893ee819c39e38c2fd9d83fd284a4
Andrew Downs <andrewdownsau@gmail.com>
2020-10-19 16:12:50 +1100

    Changed app.js text for netlify deployment test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing netlify deployment
+          Testing netlify deployment test 2
         </p>
         <a
           className="App-link"
