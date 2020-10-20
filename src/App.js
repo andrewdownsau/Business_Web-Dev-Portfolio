1e7d5df3446cd8e786cf6a767e2fefb0c27bfe7a
Andrew Downs <andrewdownsau@gmail.com>
2020-10-20 16:03:34 +1100

    Render test: Welcome function called from internal to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import './navigation_header.js';
+// import './navigation_header.js';
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
 
 function App() {
   // return (
--- src/navigation_header.js
+++ src/navigation_header.js
@@ -1,5 +1,5 @@
-import React from 'react';
+// import React from 'react';
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
-}
+// function Welcome(props) {
+//   return <h1>Hello, {props.name}</h1>;
+// }
