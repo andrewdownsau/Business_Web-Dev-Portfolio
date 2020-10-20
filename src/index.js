ad3d122c51e789c8de16b3f7be6a0c3eb3555594
Andrew Downs <andrewdownsau@gmail.com>
2020-10-20 15:54:30 +1100

    Render test: function inside index.js file called

--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@ import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const element = <h1>Hello, world</h1>;
-ReactDOM.render(element, document.getElementById('root'));
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+const element = <Welcome name="Sara" />;
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
 
 // ReactDOM.render(
 //   <React.StrictMode>
