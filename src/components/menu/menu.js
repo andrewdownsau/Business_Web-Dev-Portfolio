11cf44181f08af1effbae7f5417ff57eb9700c03
Andrew Downs <andrewdownsau@gmail.com>
2020-10-24 10:35:05 +1100

    Refactor: dropdown menu works with refactor, css responsive code now separated by component

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -8,32 +8,19 @@ import external_links from './external_links.json';
 
   
 export default function render(props) {
-  const external_links_width = String(external_links.length * 60)
+  const external_links_width = String(external_links.length * 65)
   return (
     <header>
       <nav id="top_nav">
         <section id="page_links" style={{width: 'calc(100% - ' + external_links_width + 'px)'}}>
           {render_page_links(props.page)}
+          <button type="button" className="icon" onClick={menu_dropdown_up}>
+            <i className="fa fa-bars"></i>
+          </button>
         </section>
         <section id="external_links">
           {render_external_links()}
         </section>
-        {/* <Link className={set_active(props.page, "home")} onClick={menu_un_collapse} to="/">Home</Link>
-        <Link className={set_active(props.page, "about")} onClick={menu_un_collapse} to="/about">About</Link>
-        <Link className={set_active(props.page, "skills")} onClick={menu_un_collapse} to="/skills">Skills</Link>
-        <Link className={set_active(props.page, "projects")} onClick={menu_un_collapse} to="/projects">Projects</Link>
-        <Link className={set_active(props.page, "blog")} onClick={menu_un_collapse} to="/blog">Blog</Link>
-        <Link className={set_active(props.page, "contact")} onClick={menu_un_collapse} id="contact_link" to="/contact">Contact</Link>
-
-        <a id="github_link" href="https://github.com/andrewdownsau" target="_blank" rel="noopener noreferrer">
-          <img alt="GitHub Link" src={ process.env.PUBLIC_URL + '/images/contact_links/github.svg'}/>
-        </a>
-        <a id="linkedin_link" href="https://www.linkedin.com/in/andrew-downs-5899727b/" target="_blank" rel="noopener noreferrer">
-          <img alt="LinkedIn Link" src={ process.env.PUBLIC_URL + '/images/contact_links/linkedin.svg'}/>
-        </a>
-        <button type="button" className="icon" onClick={menu_dropdown_up}>
-          <i className="fa fa-bars"></i>
-        </button> */}
       </nav>
     </header>
   );
@@ -61,12 +48,12 @@ function render_external_links() {
   );
 }
 
-// function menu_dropdown_up() {
-//   var navigation_menu = document.getElementById("top_nav");
-//   if (navigation_menu.className === "") {
-//     navigation_menu.className += "responsive";
-//   } else {
-//     navigation_menu.className = "";
-//   }
-// }
+function menu_dropdown_up() {
+  var navigation_menu = document.getElementById("top_nav");
+  if (navigation_menu.className === "") {
+    navigation_menu.className += "responsive";
+  } else {
+    navigation_menu.className = "";
+  }
+}
 
--- src/components/menu/page_link/page_link.js
+++ src/components/menu/page_link/page_link.js
@@ -5,10 +5,12 @@ import {NavLink} from "react-router-dom";
 export default function render(props){
   const page_link_width = String(100/props.number)+'%';
   const nav_key = "nav_link " + props.page;
+  const page_link_id = props.page.toLowerCase() + "_link"
   const page_link_path = '/' + props.page.toLowerCase();
   const page_link_link = (
     <NavLink 
-      key={nav_key} 
+      key={nav_key}
+      id={page_link_id}
       className="page_link" 
       style={{ width: page_link_width }} 
       onClick={menu_un_collapse} 
