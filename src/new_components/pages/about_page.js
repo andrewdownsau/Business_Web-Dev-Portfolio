94e4412df0ec7cc155b907f68ee6e5cb93f4fe6c
Andrew Downs <andrewdownsau@gmail.com>
2020-10-26 09:16:09 +1100

    Refactor: Resume link added back to about page with same styling

--- src/new_components/pages/about_page.js
+++ src/new_components/pages/about_page.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './styles/about_page.css';
 import PageTitle from '../body_components/page_title/page_title.js';
 import PageText from '../body_components/page_text/page_text.js';
 
@@ -15,6 +16,9 @@ export default class AboutPage extends React.Component {
         <PageText content="Understanding has come hard, but nothing worth doing was ever easy." />
         <PageText content="I'm a full stack web developer in training @ Coder Academy I want to understand the wants and needs of both clients and their customers to produce digital products that enrich our connections and expand our understanding." />
         <PageText content="This web portfolio is here to showcase my skills and interests to anyone who may be need of my abilities presently or in the near future. If you want to view a formal document, click on this link below." />
+        <a id="resume_link" href="/2020_02_18 Andrew Downs Resume.pdf">
+          Developer Resume
+        </a>
       </section>
     );
   }
