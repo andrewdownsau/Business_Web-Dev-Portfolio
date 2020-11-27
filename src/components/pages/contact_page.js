baa0b4a1709d0970e693106cc96266ed781118ed
Andrew Downs <andrewdownsau@gmail.com>
2020-11-27 21:37:47 +1100

    Feature(Contact Page): Added form with name, email and message fields. Connected to formspree

--- src/components/pages/contact_page.js
+++ src/components/pages/contact_page.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './styles/contact_page.css'
 import PageTitle from '../global/body_components/page_title/page_title';
 
 export default class ContactPage extends React.Component {
@@ -10,6 +11,18 @@ export default class ContactPage extends React.Component {
     return (
       <section id={this.state.page + "_page_content"}>
         <PageTitle content="If you'd like to get in touch..." />
+        <form action="https://formspree.io/f/mnqobdee" method="POST">
+            <label for="name">Name:</label>
+            <input type="text" name="name" /><br />
+            <label for="email">Email Address:</label>
+            <input type="text" name="email" /><br />
+            <label for="message">Message:</label>
+            <textarea name="message" rows="10" cols="30"></textarea><br />
+            <input id="form-submit" type="submit" value="Submit Message" />
+        </form><br />
+        <p>Alternatively, you can reach me with these methods:<br />
+            email: andrewdownsau@gmail.com<br />
+            twitter: https://twitter.com/AndrewDownsDev</p>
       </section>
     );
   }
